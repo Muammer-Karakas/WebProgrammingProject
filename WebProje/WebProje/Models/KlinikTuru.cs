47f30222e4a8b0c2a47c32a6594eeb64f6526ee1
Muammer-Karakas <103829206+Muammer-Karakas@users.noreply.github.com>
2023-12-03 22:50:02 +0300

    Mvc model oluşum
    Mvc mimarisinde model oluşturuldu.
    Bu model, EF ile veritabanı bağlantısı kuruldu.(DbContext sınıfı)
    Proje tanıtım dosyasında ufak eklemeler, düzenlemeler yapıldı.

---
+++ WebProje/WebProje/Models/KlinikTuru.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProje.Models
+{
+    public class KlinikTuru
+    {
+        [Key]
+        public int KlinikTuruId { get; set; }
+
+        [Required]
+        public string Ad {  get; set; }
+    }
+}
---
+++ WebProje/WebProje/Utility/HastaneRandevuDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebProje.Models;
+
+namespace WebProje.Utility
+{
+    public class HastaneRandevuDbContext : DbContext
+    {
+        public HastaneRandevuDbContext(DbContextOptions<HastaneRandevuDbContext> options) : base(options) { }
+
+        public DbSet<KlinikTuru> KlinikTurleri { get; set; }
+        
+    }
+}
