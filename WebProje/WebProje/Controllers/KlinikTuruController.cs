976fa7acc6fe015dd289b4a819ded4fc1c63eaa7
Muammer-Karakas <103829206+Muammer-Karakas@users.noreply.github.com>
2023-12-06 00:21:16 +0300

    Controller , View ilk oluşum
    MVC modelden sonra Controller ve view oluşturulmaya başlandı. Dependency injection kullanıldı. Veritabanına veri yüklendi ,Çeşitli düzenlemeler yapıldı.

---
+++ WebProje/WebProje/Controllers/KlinikTuruController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProje.Models;
+using WebProje.Utility;
+
+namespace WebProje.Controllers
+{
+    public class KlinikTuruController : Controller
+    {
+        private readonly HastaneRandevuDbContext _hastaneRandevuDbContext;
+
+        public KlinikTuruController(HastaneRandevuDbContext context)
+        {
+            _hastaneRandevuDbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<KlinikTuru>NsnKlinikTuruList= _hastaneRandevuDbContext.KlinikTurleri.ToList();
+            return View(NsnKlinikTuruList);
+        }
+    }
+}
